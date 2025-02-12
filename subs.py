6bd7523b073aacc64ee5fba17d331bde9a64fd97
HubertLapka <126490401+HubertLapka@users.noreply.github.com>
2025-02-12 13:07:14 +0100

    Finding a Motif in DNA

---
+++ subs.py
@@ -0,0 +1,10 @@
+haystack = "GATATATGCATATACTT"
+needle = "ATAT"
+occurences = []
+
+for x in range(len(haystack)-len(needle)):
+    if haystack[x:len(needle)+x] == needle:
+        occurences.append(x+1)
+        
+for el in occurences:
+    print(str(el), end=' ')
